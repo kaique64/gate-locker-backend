ba696331b96207ab6d9f51fb70e3294bc7d1013d
kaique64 <kaiquehenrique@outlook.com>
2022-11-19 23:10:13 -0300

    connect MQTT

--- gatelocker/api/views.py
+++ gatelocker/api/views.py
@@ -1,5 +1,6 @@
 import os
 import random
+from paho.mqtt import client as mqtt_client
 
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import csrf_protect
@@ -8,16 +9,30 @@ from rest_framework.decorators import api_view
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-CLIENT_ID = f'python-mqtt-{random.randint(0, 100000)}'
+BROKER_URL = os.getenv('BROKER_URL')
+BROKER_PORT = os.getenv('BROKER_PORT')
+BROKER_TOPIC = os.getenv('BROKER_TOPIC')
+
+class MQTTConnection:
+    def connect(self, url, port):
+        client_id = f'python-mqtt-{random.randint(0, 100000)}'
+
+        def on_connect(client, userdata, flags, rc):
+            if rc != 0:
+                print("Failed to connect, return code %d\n", rc)
+            print("Connected to MQTT Broker!")
+        
+        client = mqtt_client.Client(client_id)
+        client.on_connect = on_connect
+        client.connect(url, port)
+        return client
+
 
 class GateViewSet(viewsets.ModelViewSet):
     @api_view(['POST'])
     @csrf_protect
     @csrf_exempt
     def open_gate(self):
-        print(os.getenv('BROKER_URL'))
-        print(os.getenv('BROKER_PORT'))
-        print(os.getenv('BROKER_TOPIC'))
         return Response({
             "status": 200,
             "open": True,
